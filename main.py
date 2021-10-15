4aa721f751c79d28349a975913f131646b3fe984
David Brown <dbrown92700@gmail.com>
2021-10-15 12:16:03 -0400

    Added more error handling for vManage Login

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ __contributors__ = []
 __copyright__ = "Copyright (c) 2012 Cisco and/or its affiliates."
 __license__ = "Cisco Sample Code License, Version 1.1"
 
-from flask import Flask, request, make_response, render_template, redirect, url_for, session
+from flask import Flask, make_response, render_template, redirect, url_for, session
 from markupsafe import Markup
 from includes import *
 from json2html import *
@@ -26,31 +26,36 @@ import json
 app = Flask(__name__)
 app.secret_key = 'any random string'
 
+
 ###########################################################################
 #  Prompt user to set vManage settings
 ###########################################################################
+
 @app.route('/')
 def getsettings():
     vmanage = request.cookies.get('vmanage')
     userid = request.cookies.get('userid')
     password = request.cookies.get('password')
-    if vmanage == None:
+    if vmanage is None:
         vmanage = userid = password = 'not set'
-    return render_template('getsettings.html', vmanage=vmanage, userid=userid, password=password, secret='*****'+password[-2:])
+    return render_template('getsettings.html', vmanage=vmanage, userid=userid, password=password,
+                           secret='*****' + password[-2:])
+
 
 ###########################################################################
 #  Read and save settings
 ###########################################################################
+
 @app.route('/savesettings')
 def savesettings():
-
     resp = make_response(redirect(url_for('menu')))
 
-    ### Save vManage settings in a cookie:
+    # Save vManage settings in a cookie
     for arg in request.args:
         resp.set_cookie(arg, request.args.get(arg), secure=True, httponly=True)
 
     return resp
+
 
 ###########################################################################
 #  Main menu.  This screen also clears any leftover session variables
@@ -58,21 +63,16 @@ def savesettings():
 
 @app.route('/menu')
 def menu():
-
-    ### Clear user session variables from previous tasks
+    # Clear user session variables from previous tasks
     session.clear()
     try:
         vmanage = login()
-    except:
-        return Markup(f'''Error attempting to log in.  Make sure:<br>
-                      <ul><li>vManage is reachable from this server.
-                    <li>Your vManage IP or Name is correct (do not include "https://", etc.)
-                    <li>Your credentials have API privileges</ul>
-                    <a href="/">Edit vManage settings</a>''')
+    # Problems logging into vManage should be caught here...
+    except Exception as err:
+        return render_template('error.html', err=err)
     devices = vmanage.get_request('system/device/vedges')
     vmanage.logout()
-    #print(json.dumps(devices,indent=2))
-    models='<option label="all">all</option>\n'
+    models = '<option label="all">all</option>\n'
     for device in devices['data']:
         print(device['deviceModel'])
         if device['deviceModel'] not in models:
@@ -80,55 +80,56 @@ def menu():
     print(models)
     return render_template('menu.html', vmanage=request.cookies.get('vmanage'), models=Markup(models))
 
+
 ###########################################################################
 #  List edges.  Takes parameters model and mode.
 ###########################################################################
 
 @app.route('/listedges')
 def listedges():
-
     model = request.args.get('model') or 'all'
     mode = request.args.get('mode') or 'all'
 
     vmanage = login()
-    data = list_edges(vmanage, mode = mode, model = model)
+    data = list_edges(vmanage, mode=mode, model=model)
     vmanage.logout()
-    data.insert(0, ['UUID','Hostname', 'Model','Mode'])
+    data.insert(0, ['UUID', 'Hostname', 'Model', 'Mode'])
     output = buildtable(data)
 
     return render_template('table.html', title='List Edges', instructions='List of all edge devices',
                            data=Markup(output))
 
+
 ###########################################################################
 #  List templates.  Takes parameter model.
 ###########################################################################
+
 @app.route('/listtemplates')
 def listtemplates():
-
     model = request.args.get('model') or 'all'
     vmanage = login()
     data = list_templates(vmanage, model)
     vmanage.logout()
-    data.insert(0, ['UUID','Name', 'Description','Device Type'])
+    data.insert(0, ['UUID', 'Name', 'Description', 'Device Type'])
     output = buildtable(data)
 
     return render_template('table.html', title='List Templates', instructions='List of all templates',
                            data=Markup(output))
+
 
 ###########################################################################
 #  RMA Edge. Collects device to replace, new device, and template details
 ###########################################################################
 @app.route('/rmaedge')
 def rmaedge():
-
     model = request.args.get('model') or session['model']
     session['model'] = model
-    ### List edges in vManage mode for user to select from
-    ### If oldedge is already set move to the next step.
+    # List edges in vManage mode for user to select from
+    # If oldedge is already set move to the next step.
     try:
         oldedge = request.args.get('oldedge') or session['oldedge']
         session['oldedge'] = oldedge
-    except:
+    except KeyError:
         vmanage = login()
         data = list_edges(vmanage, mode='vmanage', model=model)
         data.insert(0, ['UUID', 'Hostname', 'Model', 'Mode'])
@@ -137,12 +138,12 @@ def rmaedge():
         return render_template('table.html', data=Markup(output), title='Pick Old Edge',
                                instructions=Markup('Select the Edge device to replace:<br><br>'))
 
-    ### List edges in CLI mode for user to choose from.
-    ### If replacement edge is already set, move to the next step.
+    # List edges in CLI mode for user to choose from.
+    # If replacement edge is already set, move to the next step.
     try:
         newedge = request.args.get('newedge') or session['newedge']
         session['newedge'] = newedge
-    except:
+    except KeyError:
         vmanage = login()
         model = vmanage.get_request(f'device/models/{oldedge}')['name']
         session['model'] = model
@@ -162,41 +163,41 @@ def rmaedge():
     session['template'] = template
     vmanage.logout()
     jtemplate = Markup(json2html.convert(template))
-    return render_template('rmaconfirm.html',template=jtemplate, oldedge=oldedge, newedge=newedge)
+    return render_template('rmaconfirm.html', template=jtemplate, oldedge=oldedge, newedge=newedge)
+
 
 ###########################################################################
 #  RMA Edge confirmation screen prompts for confirmation and executes exchange
 ###########################################################################
 @app.route('/rmaconfirm')
 def rmaconfirm():
-
     #
-    ### Deletes oldedge, attaches template to newedge, returns job result
+    # Deletes oldedge, attaches template to newedge, returns job result
     #
 
-    ### Invalidate Device Certificate
+    # Invalidate Device Certificate
     vmanage = login()
     cert_status = set_certificate(vmanage, session['oldedge'], session['model'], 'invalid')
     output = '<b>Invalidate Certificate:</b><br>'
     output += str(cert_status)
 
-    ### Delete old device
+    # Delete old device
     delete_status = vmanage.delete_request(f'system/device/{session["oldedge"]}')
     output += '<br><b>Delete Edge:</b><br>'
     output += str(delete_status)
 
-    ### Create template variables JSON object with new UUID
+    # Create template variables JSON object with new UUID
     template = session['template']
     template['device'][0]['csv-deviceId'] = session['newedge']
-    payload = {"deviceTemplateList":[
+    payload = {"deviceTemplateList": [
         template
     ]
     }
     output += '<br><b>Build template payload</b><br>'
-    output += (json.dumps(payload,indent=2))
+    output += (json.dumps(payload, indent=2))
 
-    ### Attach template to new edge
-    attach_job = vmanage.post_request('template/device/config/attachment', payload = payload)
+    # Attach template to new edge
+    attach_job = vmanage.post_request('template/device/config/attachment', payload=payload)
     output += '<br><b>Attach Template:</b><br>'
     output += str(attach_job)
     output += action_status(vmanage, attach_job['id'])
@@ -211,15 +212,14 @@ def rmaconfirm():
 ###########################################################################
 @app.route('/editedge')
 def editedge():
-
     model = request.args.get('model') or session['model']
     session['model'] = model
-    ### Build a table of edges for user to select from.
-    ### If edge has already been set, move to next step.
+    # Build a table of edges for user to select from.
+    # If edge has already been set, move to next step.
     try:
         edge = request.args.get('edge') or session['edge']
         session['edge'] = edge
-    except:
+    except KeyError:
         vmanage = login()
         data = list_edges(vmanage, mode='vmanage', model=model)
         data.insert(0, ['UUID', 'Hostname', 'Model', 'Mode'])
@@ -228,15 +228,18 @@ def editedge():
         return render_template('table.html', data=Markup(output), title='Edit Edge Values',
                                instructions=Markup('Select the Edge device to edit:<br><br>'))
 
-
-    ### Build a form of template variables for user to edit.
-    ### Uses templateId parameter or finds attached templateId
-    ### Post form to update template
+    # Build a form of template variables for user to edit.
+    # Uses templateId parameter or finds attached templateId
+    # Post form to update template
     vmanage = login()
     try:
         templateId = request.args.get('templateId') or session['templateId']
-        template = get_device_template_variables(vmanage, edge, templateId)
-    except:
+        try:
+            template = get_device_template_variables(vmanage, edge, templateId)
+        # If user does not have Write privileges, error will be caught here
+        except Exception as err:
+            return render_template('error.html', err=err)
+    except KeyError:
         template = get_device_template_variables(vmanage, edge)
     vmanage.logout()
     session['template'] = template
@@ -251,32 +254,33 @@ def editedge():
     output = buildtable(tabdata)
     output += buildform(formdata, action='/updatetemp')
     return render_template('table.html', data=Markup(output), title='Edit Edge Values',
-                           instructions=Markup('Edit any values below and submit to update the device configuration:<br><br>'))
+                           instructions=Markup(
+                               'Edit any values below and submit to update the device configuration:<br><br>'))
+
 
 ###########################################################################
 #  Attach template and monitor job result
 ###########################################################################
 @app.route('/updatetemp', methods=['POST'])
 def updatetemp():
-
-    ### Retrieve variables and modify template
+    # Retrieve variables and modify template
     template = session['template']
     output = '<A HREF="/menu">Return to Main Menu.</A><BR>'
     output += 'Old Template:<BR>' + json2html.convert(template)
 
-    ### Create template variables JSON object with new UUID
+    # Create template variables JSON object with new UUID
     variables = request.form
     for value in variables:
         template['device'][0][value] = variables[value]
-    payload = {"deviceTemplateList":[
+    payload = {"deviceTemplateList": [
         template
     ]
     }
     output += "<BR>New Template:<BR>" + json2html.convert(payload)
 
-    ### Attach template to new edge
+    # Attach template to new edge
     vmanage = login()
-    attach_job = vmanage.post_request('template/device/config/attachment', payload = payload)
+    attach_job = vmanage.post_request('template/device/config/attachment', payload=payload)
     output += '<br><b>Attach Template:</b><br>'
     output += str(attach_job)
     output += action_status(vmanage, attach_job['id'])
@@ -292,15 +296,14 @@ def updatetemp():
 ###########################################################################
 @app.route('/deployedge')
 def deployedge():
-
-    ### List edges in CLI mode for user to choose from.
-    ### If replacement edge is already set, move to the next step.
+    # List edges in CLI mode for user to choose from.
+    # If replacement edge is already set, move to the next step.
     model = request.args.get('model') or session['model']
     session['model'] = model
     try:
         edge = request.args.get('edge') or session['edge']
         session['edge'] = edge
-    except:
+    except KeyError:
         vmanage = login()
         data = list_edges(vmanage, mode='cli', model=model)
         data.insert(0, ['UUID', 'Hostname', 'Model', 'Mode'])
@@ -312,14 +315,16 @@ def deployedge():
         return render_template('table.html', data=Markup(output), title='Pick New Edge',
                                instructions=Markup('Select the replacement Edge:<br><br>'))
 
-    ### Build a list of templates that apply to the edge deviceType for the user to choose from
-    ### Send the templateId and deviceId to the Edit Edge routine
+    # Build a list of templates that apply to the edge deviceType for the user to choose from
+    # Send the templateId and deviceId to the Edit Edge routine
     vmanage = login()
     data = list_templates(vmanage, model=session['model'])
     vmanage.logout()
     data.insert(0, ['uuid', 'Name', 'Description', 'device type'])
     output = buildtable(data, link='/editedge?templateId=')
-    return render_template('table.html', data=Markup(output), title='Pick a template', instructions=Markup('Select the template to apply:<br><br>'))
+    return render_template('table.html', data=Markup(output), title='Pick a template',
+                           instructions=Markup('Select the template to apply:<br><br>'))
+
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
